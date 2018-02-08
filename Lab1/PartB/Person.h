a6d57b52cd3f107bfc08ee4ed623b3c8c0df48bb
Michael Gallagher <mikey96g@hotmail.com>
2018-02-08 22:56:19 +0000

    part b

--- Lab1/PartB/Person.h
+++ Lab1/PartB/Person.h
@@ -1,1 +1,22 @@
 #pragma once
+
+#ifndef Products_h
+#define Products_h
+
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+class Person
+{
+public:
+	Person(string); // initialise the name
+	virtual void printname();
+protected:
+	string name;
+};class Employee : public Person{public:	void printname();private:	double salary;};class Customer: public Customer{public:	void printname();private:	string complaint;};
+
+
+#endif
