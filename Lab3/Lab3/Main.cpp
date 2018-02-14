d691c17c1d2e4d9b62d8a0b8eff33fcf0335c41b
Michael Gallagher <mikey96g@hotmail.com>
2018-02-14 17:49:23 +0000

    Part1

---
+++ Lab3/Lab3/Main.cpp
@@ -0,0 +1,34 @@
+#include <iostream> 
+#include <exception>
+
+
+void mergeSort(int arraryToSort[],int startIndex, int length)
+{
+	for (int i = 0;i <= length;i++)
+	{
+		if (arraryToSort[i] > length && arraryToSort[i] <startIndex)
+		{
+			throw std::exception();
+		}
+	}
+
+	//avoids overflow for large l and h
+	if(startIndex < length)
+	{
+		int m = 1 + (length - 1) / 2;
+
+		//Sort first and second halves
+		mergeSort(arraryToSort, startIndex, m);
+		mergeSort(arraryToSort, m + 1, length);
+
+		merge(arraryToSort, startIndex, length, m);
+	}
+
+}
+
+void merge(int arrayMerge[],int start,int end, int m) {};
+
+int main()
+{
+}
+
