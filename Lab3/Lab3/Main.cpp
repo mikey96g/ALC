fe372891c26f9bc9214839db2fc624c97fc86ba4
Michael Gallagher <mikey96g@hotmail.com>
2018-02-14 18:54:39 +0000

    arrarys

--- Lab3/Lab3/Main.cpp
+++ Lab3/Lab3/Main.cpp
@@ -1,7 +1,18 @@
 #include <iostream> 
 #include <exception>
 
-void merge(int arrayMerge[], int start, int end, int m) {};
+void merge(int arraySortedInTwoHalves[], int startIndex, int length) 
+{
+	int m = 1 + (length - 1) / 2;
+	int i, j, k;
+	int n1 = m - startIndex + 1;
+	int n2 = length - m;
+
+	//temp arrarys
+	int *temp1 = new int[n1];
+	int *temp2 = new int[n2];
+
+};
 
 void mergeSort(int arraryToSort[],int startIndex, int length)
 {
@@ -13,16 +24,17 @@ void mergeSort(int arraryToSort[],int startIndex, int length)
 		}
 	}
 
-	//avoids overflow for large l and h
+	
 	if(startIndex < length)
 	{
+		//avoids overflow for large l and h
 		int m = 1 + (length - 1) / 2;
 
 		//Sort first and second halves
 		mergeSort(arraryToSort, startIndex, m);
 		mergeSort(arraryToSort, m + 1, length);
 
-		merge(arraryToSort, startIndex, length, m);
+		merge(arraryToSort, startIndex, length);
 	}
 
 }
