15f5eb0a86a24a93fb8b46528a4ceb429453b495
michael <mikey96g@hotmail.com>
2018-02-16 15:51:03 +0000

    Quicksort done, remember to comment and references

--- Lab3/Lab3/Main.cpp
+++ Lab3/Lab3/Main.cpp
@@ -55,10 +55,58 @@ void mergeSort(int iArray[], int startIndex, int endIndex)
 
 }
 
+//swap for pointers
+void swap(int* a,int* n)
+{
+	int t = *a;
+	*a = *n;
+	*n = t;
+}
+
+int quickSortDivide(int theArray[], int first, int last)
+{
+	int pivot = theArray[last];//pivot
+	int i = (first - 1); //Index of smaller element
+
+	for (int j = first; j<= last - 1; j++)
+	{
+		if(theArray[j]<=pivot)
+		{
+			i++;
+			swap(&theArray[i], &theArray[j]);						
+		}
+	}
+	swap(&theArray[i + 1], &theArray[last]);
+	return (i + 1);
+}
+
+void quickSort(int theArray[], int first, int last)
+{
+	if(first < last)
+	{
+		/*pi is partioning index, theArray[p] is now at the right place */
+		int pi = quickSortDivide(theArray, first, last);
+
+		/*Separately sort elements before divide and after divide*/
+		quickSort(theArray,first,pi -1);
+		quickSort(theArray, pi+1,last);
+
+	}
+}
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+	int i;
+	for (i = 0; i < size; i++)
+		printf("%d ", arr[i]);
+	printf("\n");
+}
+
+
 
 int main()
 {
-	int arr[10] = { 2,5,6,4,7,2,8,3,9,10 };
+	/*int arr[10] = { 2,5,6,4,7,2,8,3,9,10 };
 	
 
 	for (int i = 0; i < 10; i++)
@@ -69,7 +117,13 @@ int main()
 	for (int i = 0; i < 10; i++)
 	{
 		std::cout << arr[i] << "\n\n ";
-	}
+	}*/
+
+	int arr[] = { 10, 7, 8, 9, 1, 5 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	quickSort(arr, 0, n - 1);
+	printf("Sorted array: \n");
+	printArray(arr, n);
 
 	std::cout << "\n\n";
 	system("pause");
